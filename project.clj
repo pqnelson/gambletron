1f0100230395884911ebd24ac5200b3720e7cbc0
Alex Nelson <pqnelson@gmail.com>
2015-09-05 11:09:59 -0700

    [minor] Upgraded packages to latest versions

--- project.clj
+++ project.clj
@@ -4,15 +4,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
-                 [clj-time "0.10.0"]
-                 [net.mikera/core.matrix "0.36.1"]
+                 [clj-time "0.11.0"]
+                 [net.mikera/core.matrix "0.41.0"]
                  [incanter "1.9.0"]
-                 [com.taoensso/timbre "4.0.2"] ; profiling purposes
+                 [com.taoensso/timbre "4.1.2"] ; profiling purposes
                  [org.clojure/math.combinatorics "0.1.1"]
+                 [weka/weka "3.6.2"]
                  
                  ;; database packages
                  [korma "0.4.2"]
-                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
 
                  ;; LOGGING DEPS
