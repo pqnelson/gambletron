b04145cc7ed83c93c1d25167086778d6a2341511
Alex Nelson <pqnelson@gmail.com>
2016-01-24 13:38:21 -0800

    Added more event helper functions

--- src/gambletron/events.clj
+++ src/gambletron/events.clj
@@ -1,7 +1,12 @@
 (ns gambletron.events
-  (:require [korma.core :refer [select where] :as korma]
-            [gambletron.schema :refer [events]]
+  (:refer-clojure :exclude [update])
+  (:require [clojure.string :as string]
+            (clj-time [coerce :as coerce]
+                      [core :as time])
+            [korma.core :refer [select where] :as korma]
+            [gambletron.schema :refer [events games vw_events vw_games]]
             [gambletron.util :refer [defconst] :as util]
+            [gambletron.pitching :as pitching]
             [gambletron.player :as player]
             [gambletron.stats.util :as stats-util]))
 
@@ -18,6 +23,78 @@
           (where {:game-id game-id})
           (korma/order :event-id :asc)))
 
+(defn find-all-since [time-dt]
+  (select vw_events
+          (where {:game-date [> (coerce/to-sql-date time-dt)]})))
+
+(defn find-start-pitcher-events [game-id]
+  (select events
+          (where {:game-id game-id
+                  :pit-start-fl "T"})))
+
+(defconst national-team-ids ["ARI" "ATL" "CHN" "CIN" "COL" "LAN" "MIA" "MIL" "NYN" "PHI" "PIT" "SDN" "SFN" "SLN" "WAS" "FLO" "MON"])
+(defconst american-team-ids ["ANA" "BAL" "BOS" "CHA" "CLE" "DET" "HOU" "KCA" "MIN" "NYA" "OAK" "SEA" "TBA" "TEX" "TOR" "CAL"])
+
+(defn find-foo
+  ([]
+   (korma/exec-raw [(str "SELECT outs_ct, start_bases_cd, inn_runs_ct, fate_runs_ct, game_id, event_id, bat_id "
+                         "FROM vw_events "
+                         "WHERE year>2010 "
+                        "AND ((away_team_id IN ("
+                        (string/join "," (map #(str "'" % "'") american-team-ids))
+                        ")) AND (home_team_id IN ("
+                        (string/join "," (map #(str "'" % "'") american-team-ids))
+                        ")))"
+                        )
+                   []]
+                   :results))
+  ([league]
+   (korma/exec-raw [(str "SELECT outs_ct, start_bases_cd, inn_runs_ct, fate_runs_ct, game_id, event_id, bat_id "
+                         "FROM vw_events "
+                         "WHERE year>2010 "
+                         "AND ((away_team_id IN ("
+                         (string/join "," (map #(str "'" % "'") league))
+                         ")) AND (home_team_id IN ("
+                         (string/join "," (map #(str "'" % "'") league))
+                         ")))"
+                         )
+                    []]
+                   :results)))
+
+(defn- group-into-half-innings [game-events]
+  (->> game-events
+       (partition-by #(and (zero? (:outs-ct %)) (= "0" (:start-bases-cd %))))
+       (partition-all 2)
+       (map #(apply concat %))))
+
+(defn- assoc-runs-scored [game-events]
+  (for [half-inning (group-into-half-innings game-events)
+        event half-inning]
+    (assoc event :runs ;; (+ (:inn-runs-ct (last half-inning))
+                       ;;    (:fate-runs-ct (last half-inning)))
+           (+ 0 ;; (:inn-runs-ct event)
+              (:fate-runs-ct event))
+           ;; (:fate-runs-ct event)
+           )))
+       
+(defn- remove-duplicate [game-events]
+  (loop [[head & remaining] (sort-by :event-id game-events)
+         acc []]
+    (if (empty? remaining)
+      (if head
+        (conj acc head)
+        acc)
+      (if (= (:bat-id head) (:bat-id (first remaining)))
+        (recur remaining acc)
+        (recur remaining (conj acc head))))))
+
+(defn filter-events [e]
+  (->> e
+       (map #(assoc % :event-id (util/parse-int (:event-id %))))
+       (group-by :game-id)
+       (mapcat (fn [[_ game-events]]
+                 (assoc-runs-scored (remove-duplicate game-events))))))
+
 (defconst get-batter-id :bat-id)
 (defconst get-pitcher-id :pit-id)
 
@@ -32,6 +109,35 @@
 (defn year [event-map]
   (read-string (subs (:game-id event-map) 3 7)))
 
+(defn initial-status [event-map]
+  (+ (* 8 (:outs-ct event-map))
+     (or (util/parse-int (:start-bases-cd event-map)) 0)))
+
+(defn find-bar []
+  (korma/exec-raw [(str "SELECT outs_ct, start_bases_cd, inn_runs_ct, count(*) "
+                        "FROM vw_events "
+                        "WHERE year>2000 "
+                        "GROUP BY outs_ct, start_bases_cd, inn_runs_ct")
+                   []]
+                  :results))
+
+;; (frequencies (map (juxt initial-status :rbi-ct)
+
+(defn final-status [event-map]
+  (+ (* 8 (:event-outs-ct event-map))
+     (or (util/parse-int (:end-bases-cd event-map)) 0)))
+
+;; treat various flags as predicates
+(defn- flag-predicate [flag-key event-map]
+  (when-not (string/blank? (flag-key event-map))
+    (= "T" (flag-key event-map))))
+
+(defn pinch-hit? [event-map]
+  (flag-predicate :ph-flag event-map))
+
+(defn starter-pitcher? [event-map]
+  (flag-predicate :pit-start-fl event-map))
+
 ;; helper function to parse chadwick event conditions
 (defn- event-type-eq? [code event-map]
   (if (set? code)
@@ -40,7 +146,16 @@
 
 ;; http://chadwick.sourceforge.net/doc/cwevent.html#cwtools-cwevent-eventtype
 (defn out? [event-map]
-  (event-type-eq? #{2 3 6 7 8} event-map))
+  (event-type-eq? #{2  ; generic out
+                    3  ; strikeout
+                    6  ; caught stealing
+                    8} ; pickoff
+                  event-map))
+
+(defn error? [event-map]
+  (event-type-eq? #{7   ; pickoff error
+                    18} ; error
+                  event-map))
 
 (defn hit? [event-map]
   (event-type-eq? #{20 ; single
@@ -55,17 +170,24 @@
 (defn walk? [event-map]
   (event-type-eq? #{14 15} event-map))
 
+(defn run-score [event]
+  (condp = (:event-cd event)
+    20 1
+    21 2
+    22 3
+    23 4
+    0))
+
 (defn hit-by-pitch? [event-map]
   (event-type-eq? 16 event-map))
 
 (defn advance? [event-map]
-  (event-type-eq? #{11  ; balk
-                    12  ; other advance
-                    14  ; walk
-                    15  ; intentional walk
-                    16  ; hit by pitch
-                    18} ; error
-                  event-map))
+  (or (walk? event-map)
+      (hit-by-pitch? event-map)
+      (error? event-map)
+      (event-type-eq? #{11  ; balk
+                        12} ; other advance
+                      event-map)))
 
 (defn on-base? [event-map]
   (or (advance? event-map)
@@ -77,16 +199,84 @@
     (out? event-map)       1/100
     :else nil))
 
-(defn- get-league-avg* [event-map]
-  (let [p (pitching/find-by-player-year (get-pitcher-id event-map)
-                                        (year event-map))]
-    (pitching/league-average-obp p)))
+(defn assoc-prob [fr]
+  (for [row fr
+        :let [N (reduce + (map :count (filter #(= ((juxt :outs-ct
+                                                     :start-bases-cd) row)
+                                              ((juxt :outs-ct
+                                                     :start-bases-cd) %))
+                                          fr)))
+              p (/ (:count row) N)]]
+    (dissoc (assoc row :probability (float p)
+                   :expected-runs (float (* (:runs row) p)))
+            :count)))
 
-(def get-league-avg (util/soft-memoize get-league-avg*))
+(defn parse-bases [base-str]
+  (condp = base-str
+    "0" "None"
+    "1" "(1)"
+    "2" "(2)"
+    "3" "(1,2)"
+    "4" "(3)"
+    "5" "(1,3)"
+    "6" "(2,3)"
+    "7" "(1,2,3)"
+    (throw (Error. (str "Unknown base condition: " base-str)))))
 
-(defn inputs [event-map]
-  [(double (e->obp event-map))
-   (double (e->obpa event-map))
-   (double (get-league-avg event-map))])
+(defn make-table [fr]
+  (for [[[o b] vs] (group-by (juxt :outs-ct :start-bases-cd) fr)]
+    {:outs o
+     :bases (parse-bases b)
+     :expected-runs (reduce + (map :expected-runs vs))}))
 
-;; (def lm1 (stats/linear-model (map (comp stats-util/logit outcome) e2) (map inputs e2) :intercept false))
+
+(defconst base-txt ["None" "(1)" "(2)" "(3)" "(1,2)" "(1,3)" "(2,3)" "(1,2,3)"])
+
+(defn make-row [t]
+  (string/join "\n"
+  (for [outs (range 3)
+        :let [row (filter #(= outs (:outs %)) t)]]
+    (string/join " | "
+                 (cons (str "| " outs)
+               (for [base base-txt
+                     :let [entry (first (filter #(= base (:bases %)) row))]]
+                 (format "%.3f" (float (:expected-runs entry)))))))))
+
+(defn re->table [e]
+  (->> e
+       filter-events
+       (map #(select-keys % [:outs-ct :start-bases-cd :runs]))
+       frequencies
+       (map (fn [[m n]] (assoc m :count n)))
+       assoc-prob
+       make-table
+       make-row
+       println))
+
+(defn avg-fail-rate [pitcher-events]
+  (->> pitcher-events
+       (partition-by advance?)
+       (map count)
+       util/mean))
+
+(defn sd-fail-rate [pitcher-events]
+  (if-not (some advance? pitcher-events)
+    0
+    (->> pitcher-events
+         (partition-by advance?)
+         (map count)
+         util/standard-dev)))
+
+(defn make-bf-outs-pairs [events]
+  (for [pitcher-id (set (map :pit-id events))
+        :let [pitcher-events (sort-by (comp util/parse-int :event-id) (filter #(= (:pit-id %) pitcher-id)
+                                     events))]
+        :when (> (count pitcher-events) 2)]
+    [(count pitcher-events)
+     (avg-fail-rate pitcher-events)
+     (sd-fail-rate pitcher-events)
+     (util/mean (map run-score pitcher-events))]))
+
+(defn foo [g1]
+  (mapcat (fn [g] (make-bf-outs-pairs (find-start-pitcher-events
+                                       (:game-id g)))) g1))
